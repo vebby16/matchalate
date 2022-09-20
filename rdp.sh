6415511ccd00b15c031db29b1443771f83971731
vebby16 <113167782+vebby16@users.noreply.github.com>
2022-09-20 23:08:21 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -102,7 +102,7 @@ tar -xvf DE.tar.gz
 cd
 mkdir Desktop
 cd Desktop
-wget https://github.com/mazbron/x/raw/master/fmb/chr.sh
+wget https://github.com/anangpurnama/anang/raw/main/chr.sh
 chmod +x chr.sh
 wget https://github.com/anangpurnama/anang/raw/main/new/gp.ascr
 wget https://github.com/anangpurnama/anang/raw/main/gpclick.ascr
