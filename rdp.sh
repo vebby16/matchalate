2a59f78964b8d8f307fa9a3e93bdaa6ce736a858
vebby16 <113167782+vebby16@users.noreply.github.com>
2022-12-10 03:02:15 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -51,7 +51,7 @@ sleep 1
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update -y
-sudo apt-get --only-upgrade install google-chrome-stable -y
+sudo apt-get install google-chrome-stable -y
 wget --no-check-certificate 'https://www.googleapis.com/drive/v3/files/1h7JJqMLu1IOGNF7OBkvt65a64C9XOhYc?alt=media&key=AIzaSyDTN35uWgTNDGpBi9iFpBf1WItEZZlr6aw' -O chrome60.deb
 sudo apt purge google-chrome-stable -y
 sleep 1
